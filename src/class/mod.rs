8b7d8dbdaa25f3ff71050a22ab490b4105340a64
Geoff Shannon <geoffpshannon@gmail.com>
2018-12-22 04:57:20 -0800

    Reorganize and add main interface def macro stubs

---
+++ src/class/mod.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+use lazy_static::lazy_static;
+
+use super::defclass;
+
+lazy_static! {
+    static ref CLASSES: RwLock<HashMap<String, Class>> = {
+        RwLock::new(HashMap::new())
+    };
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Slot {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Method {}
+
+#[derive(Clone, Debug)]
+pub struct Class {
+    inner: Arc<RwLock<InnerClass>>,
+}
+
+impl PartialEq for Class {
+    fn eq(&self, other: &Class) -> bool {
+        Arc::ptr_eq(&self.inner, &other.inner)
+    }
+}
+
+impl Eq for Class {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct InnerClass {
+    name: String,
+    direct_superclasses: Vec<Class>,
+    direct_slots: Vec<Slot>,
+    class_precedence_list: Vec<Class>,
+    effective_slots: Vec<Slot>,
+    direct_subclasses: Vec<Class>,
+    direct_methods: Vec<Method>,
+}
+
+pub fn find_class(name: impl AsRef<str>) -> Option<Class> {
+    CLASSES.read().unwrap().get(name.as_ref()).cloned()
+}
+
+pub fn ensure_class(name: String, superclasses: Vec<Class>, slots: Vec<Slot>) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(None, find_class("hello"));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,20 @@
 #![allow(dead_code, unused_variables)]
 
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+mod class;
 
-use lazy_static::lazy_static;
+mod generic;
 
-lazy_static! {
-    static ref CLASSES: RwLock<HashMap<String, Class>> = {
-        RwLock::new(HashMap::new())
-    };
+#[macro_export]
+macro_rules! defclass {
+    ($name:ident {}) => {()}
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Slot {}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Method {}
-
-#[derive(Clone, Debug)]
-struct Class {
-    inner: Arc<RwLock<InnerClass>>,
+#[macro_export]
+macro_rules! defgeneric {
+    ($name:ident () {}) => {()}
 }
 
-impl PartialEq for Class {
-    fn eq(&self, other: &Class) -> bool {
-        Arc::ptr_eq(&self.inner, &other.inner)
-    }
-}
-
-impl Eq for Class {}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct InnerClass {
-    name: String,
-    direct_superclasses: Vec<Class>,
-    direct_slots: Vec<Slot>,
-    class_precedence_list: Vec<Class>,
-    effective_slots: Vec<Slot>,
-    direct_subclasses: Vec<Class>,
-    direct_methods: Vec<Method>,
-}
-
-fn find_class(name: impl AsRef<str>) -> Option<Class> {
-    CLASSES.read().unwrap().get(name.as_ref()).cloned()
-}
-
-fn ensure_class(name: String, superclasses: Vec<Class>, slots: Vec<Slot>) {
-
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(None, find_class("hello"));
-    }
+#[macro_export]
+macro_rules! defmethod {
+    ($name:ident () {}) => {()}
 }
